c3e2c8ed232f8eda4184051889fe7e392c1e9981
Josh Nussbaum <joshnuss@gmail.com>
2021-02-20 04:11:24 -0500

    Adds Icon component

---
+++ src/Icon.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { KeyIcon, MailIcon, InboxIcon } from 'svelte-feather-icons'
+  import GoogleIcon from './social-icons/GoogleIcon.svelte'
+  import FacebookIcon from './social-icons/FacebookIcon.svelte'
+  import TwitterIcon from './social-icons/TwitterIcon.svelte'
+  import GithubIcon from './social-icons/GithubIcon.svelte'
+  import GitlabIcon from './social-icons/GitlabIcon.svelte'
+  import BitbucketIcon from './social-icons/BitbucketIcon.svelte'
+  import AzureIcon from './social-icons/AzureIcon.svelte'
+
+  const map = {
+    mail: MailIcon,
+    key: KeyIcon,
+    inbox: InboxIcon,
+    google: GoogleIcon,
+    facebook: FacebookIcon,
+    twitter: TwitterIcon,
+    github: GithubIcon,
+    gitlab: GitlabIcon,
+    bitbucket: BitbucketIcon,
+    azure: AzureIcon,
+  }
+
+  export let name
+  export let size = 10
+</script>
+
+<svelte:component this={map[name]} {size}/>
---
+++ src/social-icons/AzureIcon.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let size = 21
+</script>
+
+<svg
+  width={size}
+  aria-hidden="true"
+  focusable="false"
+  data-prefix="fab"
+  data-icon="azure-square"
+  role="img"
+  viewBox="0 0 161.67 129"
+  xmlns="http://www.w3.org/2000/svg">
+  <path d="M88.33 0L40.67 41.33 0 114.33h36.67zm6.34 9.67L74.33 67l39 49-75.66 13h124z" fill="currentColor"/>
+</svg>
