e1e316b45b4307486415f95e709a16961f2dce07
Christian Valadez <valadez.christian@gmail.com>
2021-10-14 21:44:16 +0000

    Add redirectTo in TS

--- src/index.d.ts
+++ src/index.d.ts
@@ -17,6 +17,7 @@ export interface AuthProps {
   socialLayout?: "vertical" | "horizontal";
   socialColors?: boolean;
   socialButtonSize?: "medium" | "large";
+  redirectTo?: string;
 }
 
 export default class Auth extends SvelteComponentTyped<AuthProps> {}
