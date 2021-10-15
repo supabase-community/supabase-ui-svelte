426b80efe4421e63a59364d22fc2888879779a6f
Josh Nussbaum <joshnuss@gmail.com>
2021-10-14 23:21:53 -0400

    Linting

--- rollup.config.js
+++ rollup.config.js
@@ -8,22 +8,22 @@ const name = pkg.name
   .replace(/-\w/g, m => m[1].toUpperCase())
 
 export default {
-	input: "src/index.js",
-	output: [
-		{
-			file: pkg.module,
-			sourcemap: true,
-			format: "es",
-		},
-		{
-			file: pkg.main,
-			sourcemap: true,
-			format: "umd",
-			name,
-		},
-	],
-	plugins: [
-		svelte(),
-		resolve()
-	],
-};
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.module,
+      sourcemap: true,
+      format: 'es',
+    },
+    {
+      file: pkg.main,
+      sourcemap: true,
+      format: 'umd',
+      name,
+    },
+  ],
+  plugins: [
+    svelte(),
+    resolve()
+  ],
+}
